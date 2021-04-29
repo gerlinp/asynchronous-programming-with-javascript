227214c79e101a09946972033f859069efb5f34b
gerlinp <gerlin.pierrelouis@gmail.com>
2021-04-28 22:09:42 -0400

    Promises

--- asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/promises-breakfast.js
+++ asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/promises-breakfast.js
@@ -1,9 +1,17 @@
+const order = true;
+
 const breakfastPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        reject('Oh no! There was a problem with your order.');
+        if (order) {
+            resolve('Your order is ready. Come and get it!');
+        } else {
+            reject(Error('Your order cannot be made.'));
+        }
     }, 3000);
 });
 
 console.log(breakfastPromise);
 
-breakfastPromise.then( val => console.log(val)).catch( err => console.log(err) )
+breakfastPromise
+.then( val => console.log(val))
+.catch( err => console.log(err));
