7bf316b1610cfe424621748d944305403d6f4e1f
gerlinp <gerlin.pierrelouis@gmail.com>
2021-04-28 21:43:13 -0400

    Update promises-breakfast.js

---
+++ asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/promises-breakfast.js
@@ -0,0 +1,9 @@
+const breakfastPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject('Oh no! There was a problem with your order.');
+    }, 3000);
+});
+
+console.log(breakfastPromise);
+
+breakfastPromise.then( val => console.log(val)).catch( err => console.log(err) )
